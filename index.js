72470849f3807ac629acdba3d90ba6918488c394
SheikhSalman <assalmanmuhammad@gmail.com>
2025-08-19 10:00:57 +0600

    count vowel

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 // Problem: 1, Reverse a String
-function ReverseString (string){
+function ReverseString(string) {
     return string.split("").reverse().join("")
 }
 
 const stringForReverse = "salman-koli"
-console.log(ReverseString(stringForReverse));
+const result = ReverseString(stringForReverse)
+// console.log(result);
 
 
+// Problem: 2, count vowels
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+function vowelCounter(string) {
+    let count = 0
+    for (const letter of string.toLowerCase()) {
+        // console.log(letter);
+        if (vowels.includes(letter)) {
+            count++
+        }
+    }
+    return count
+}
+
+const stringToCount = 'salman'
+const countResult = vowelCounter(stringToCount)
+// console.log(countResult);
